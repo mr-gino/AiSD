e4ec7d2ae74850714ec7374944263d28f73b16fc
mr-gino <skocz.dawid@o2.pl>
2023-11-17 15:34:50 +0100

    dodanie newton  dziel i zwyciezaj

---
+++ src/Newton_Dziel_i_zwyciezaj.java
@@ -0,0 +1,16 @@
+public class Newton_Dziel_i_zwyciezaj {
+    public static void main(String[] args)
+    {
+        final int N = 5;
+        final int K = 3;
+        System.out.println(newton(N, K));
+    }
+
+    static int newton(int n, int k)
+    {
+        if (k == 0 || k == n)
+            return 1;
+        else
+            return newton(n - 1, k - 1) + newton(n - 1, k);
+    }
+}
